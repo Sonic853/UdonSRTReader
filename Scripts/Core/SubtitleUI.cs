49619cc2183b43d7f1f2b942beb211132780c518
853 <sonic853@qq.com>
2026-06-11 19:17:48 +0800

    Move scripts

---
+++ Scripts/Core/SubtitleUI.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+namespace Sonic853.SRT
+{
+    public class SubtitleUI : UdonSharpBehaviour
+    {
+        public Text subtitle;
+        public float time;
+        void Update()
+        {
+
+        }
+    }
+}
